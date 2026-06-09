3e26ffe4b42bb0ce02f2e75eca118f28c2e20a88
priyanshi <priyanshipriya190@gmail.com>
2026-06-09 20:50:24 +0530

    Create fre_Quency.java

---
+++ fre_Quency.java
@@ -0,0 +1,15 @@
+//Count Frequency of a Character
+class Main {
+    public static void main(String[] args) {
+        String str = "programming";
+        char ch = 'm';
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ch)
+                count++;
+        }
+
+        System.out.println("Frequency = " + count);
+    }
+}
